a3de2e2bd3ca1262327a930091f7dd9da482b175
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-15 10:00:25 +0200

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 const colours = require("../colours.json");
 
 module.exports.run = async (bot, message, args) => {
-    if(!args[1]) return message.reply("Proszę zadać pytanie!");
+    if(!args[2]) return message.reply("Proszę zadać pytanie!");
     let replies = ["Tak.", "Nie.", "Nie mam pojęcia.", "Zapytaj mnie później."];
 
     let result = Math.floor((Math.random() * replies.length));
-    let question = args.slice(1).join(" ");
+    let question = args.slice(0).join(" ");
 
     let ballembed = new Discord.RichEmbed()
     .setAuthor(message.author.tag)
