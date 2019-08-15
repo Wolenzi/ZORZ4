2d4672d573db1a5db7f2da0b8f9c79ad85795c8b
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-15 08:20:25 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.replay("oof.");
     if(!args[0]) return message.channel.send("oof.");
     message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(`Usunięto ${args[0]} wiadomości`).then(msg => msg.delete(5000));
+        message.channel.send(`**Usunięto ${args[0]} wiadomości**`).then(msg => msg.delete(5000));
     })
 }
 
