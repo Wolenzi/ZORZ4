e1ebb178b70234dd8e489896dfcfcb2c3872abbf
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-15 09:44:08 +0200

    Update flipcoin.js

--- commands/flipcoin.js
+++ commands/flipcoin.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     let replies = ["Orzeł.", "Reszka.", "Moneta upadła na ziemię"];
 
     let result = Math.floor((Math.random() * replies.length));
-    let question = args.slice(1).join(" ");
+    let question = args.slice(0).join(" ");
 
     let flipembed = new Discord.RichEmbed()
     .setAuthor(message.author.tag)
