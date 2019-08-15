a972336cce3d04b95b3b373f57ce7ccdebedb529
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-15 09:35:49 +0200

    Update flipcoin.js

--- commands/flipcoin.js
+++ commands/flipcoin.js
@@ -3,7 +3,7 @@ const colours = require("../colours.json");
 
 module.exports.run = async (bot, message, args) => {
     if(!args[2]) return message.reply("Proszę rzucić!");
-    let replies = ["Orzeł.", "Reszka."];
+    let replies = ["Orzeł.", "Reszka.", "Moneta upadła na ziemię"];
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.slice(1).join(" ");
