8b34ceeac120b4d7015dcf43db81313f805bb9dc
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-15 08:02:35 +0200

    Add files via upload

---
+++ commands/clear.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.replay("oof.");
+    if(!args[0]) return message.channel.send("oof.");
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`Usunięto ${args[0]} wiadomości`).then(msg => msg.delete(5000));
+    })
+}
+
+
+module.exports.config = {
+    name: "clear",
+    aliases: ["si", "czyść"]
+}
