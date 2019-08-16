71cb8e7ce160c549b59e34c9b1441df57a90405a
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-16 17:58:03 +0200

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -2,14 +2,16 @@ const Discord = require("discord.js");
 const colours = require("../colours.json");
 
 module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return message.reply("Proszę wybrać: ``Orzeł`` lub ``Reszka``!");
-    let replies = ["Wypadło 1 oczko.", "Wypadły 2 oczka.","Wypadły 3 oczka.","Wypadły 4 oczka.","Wypadło 5 oczek.","Wypadło 6 oczek."];
+    if(!args[0]) return message.reply("Błąd");
+    let replies = ["Wypadło 1 oczko.", "Wypadły 2 oczka.","Wypadły 3 oczka.","Wypadły 4 oczka.","Wypadło 5 oczek.","Wypadło  oczek."];
 
     let result = Math.floor((Math.random() * replies.length));
+    let question = args.slice(0).join(" ");
 
     let flipembed = new Discord.RichEmbed()
     .setAuthor(message.author.tag)
     .setColor(colours.money_green)
+    .addField("Obstawiłeś", question)
     .addField("Rezultat", replies[result]);
 
     message.channel.send(flipembed);
