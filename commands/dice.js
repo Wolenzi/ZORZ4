ace6ba155afe05a92b08d0bc41c6948ff7e4c9d6
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-16 18:44:39 +0200

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js");
 const colours = require("../colours.json");
 
 module.exports.run = async (bot, message, args) => {
-    let roll = ["Wypadło 1 oczko.", "Wypadły 2 oczka.", "Wypadły 3 oczka.", "Wypadłt 4 oczka.", "Wypadło 5 oczek.", "Wypadło 6 oczek."];
-    let roll = Math.floor((Math.random() * roll));
+    let replies = ["Wypadło 1 oczko.", "Wypadły 2 oczka.", "Wypadły 3 oczka.", "Wypadłt 4 oczka.", "Wypadło 5 oczek.", "Wypadło 6 oczek."];
+    let roll = Math.floor((Math.random() * replies.length));
 
     let diceembed = new Discord.RichEmbed()
     .setAuthor(message.author.tag, message.author.avatarURL)
     .setColor(colours.money_green)
-    .addField(roll)
+    .addField(replies[roll])
 
     message.channel.send(diceembed);
 }
