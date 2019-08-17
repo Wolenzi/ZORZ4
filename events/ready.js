cf0f51fdf159beddaf313417e1bec4134ea819d0
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-17 13:06:19 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js")
 
 module.exports = bot => {
     console.log(`${bot.user.username} jest online`)
-    //bot.user.setActivity("TERRARIUM", {type: "STREAMING", url:"https://www.twitch.tv/wolzi_"});
 
     let statues = [
         `${bot.guids.size}!`,
