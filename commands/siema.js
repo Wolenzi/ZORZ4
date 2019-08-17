042c5a7d5fa48240158287b7c7998be3115cc165
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-17 22:52:18 +0200

    Update siema.js

--- commands/siema.js
+++ commands/siema.js
@@ -3,7 +3,7 @@ const botconfig = require("../botconfig.json");
 const colours = require("../colours.json");
 
 module.exports.run = async (bot, message, args) => {
-    
+    message.delete(1000);
     let siemaEmbed = new Discord.RichEmbed()
         .setDescription(`Siema **${message.author}**`)
         .setColor(colours.lime)
