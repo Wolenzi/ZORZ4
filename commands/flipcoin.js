5cddf9d6cf5f9acc857d8d39722081ed39e8651e
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-17 23:27:33 +0200

    Update flipcoin.js

--- commands/flipcoin.js
+++ commands/flipcoin.js
@@ -3,17 +3,13 @@ const colours = require("../colours.json");
 
 module.exports.run = async (bot, message, args) => {
     message.delete(1);
-    if(!args[0]) return message.reply("Proszę wybrać: ``Orzeł`` lub ``Reszka``!");
     let replies = ["Orzeł.", "Reszka."];
-
-    let result = Math.floor((Math.random() * replies.length));
-    let question = args.slice(0).join(" ");
+    let roll = Math.floor((Math.random() * replies.length));
 
     let flipembed = new Discord.RichEmbed()
     .setAuthor(message.author.tag, message.author.avatarURL)
     .setColor(colours.money_green)
-    .addField("Obstawiłeś", question)
-    .addField("Rezultat", replies[result]);
+    .addField("Rezultat:", replies[roll])
 
     message.channel.send(flipembed);
 }
