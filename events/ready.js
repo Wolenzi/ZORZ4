b89710c651951e97541eafde8f94d6653300bb97
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-17 13:03:55 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,17 @@ const Discord = require("discord.js")
 
 module.exports = bot => {
     console.log(`${bot.user.username} jest online`)
-    bot.user.setActivity("TERRARIUM", {type: "STREAMING", url:"https://www.twitch.tv/wolzi_"});
+    //bot.user.setActivity("TERRARIUM", {type: "STREAMING", url:"https://www.twitch.tv/wolzi_"});
+
+    let statues = [
+        `${bot.guids.size}!`,
+        "!siema",
+        `Ponad ${bot.users.size} użytkowników!`
+    ]
+
+    setInterval(function() {
+        let status = statues[Math.floor(Math.random() * statues.length)];
+        bot.user.setActivity(status, {type: "WATCHING"});
+
+    }, 5000)
 }
