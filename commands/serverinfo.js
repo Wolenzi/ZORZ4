30ed31c903e82f808d1937a5a99de8e52d0918af
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-08-28 21:47:06 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(message.guild.iconURL)
     .setAuthor(`Serwer ${message.guild.name}`, message.guild.iconURL)
     .addField("**Nazwa serwera:**", `${message.guild.name}`, true)
-    .addField("**Twoja nazwa:**", `${message.guild.owner}`, true)
+    .addField("**Nazwa założyciela:**", `${message.guild.owner}`, true)
     .addField("**Użytkownicy**:", `${message.guild.memberCount}`, true)
     .addField("**Role:**", `${message.guild.roles.size}`, true)
     .addField("**Utworzono:**", `${message.guild.createdAt}`, true)
