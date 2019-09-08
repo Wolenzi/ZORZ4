104e18b20e552e95856ccbd830e846438829a629
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-09-08 20:28:24 +0200

    Update eventHandler.js

--- util/eventHandler.js
+++ util/eventHandler.js
@@ -4,7 +4,6 @@ module.exports = bot => {
     bot.on("ready", function() {reqEvent("ready") (bot) });
     bot.on("reconnecting", () => reqEvent("reconnecting") (bot))
     bot.on("disconnect", () => reqEvent("disconnect") (bot))
-    bot.on("join", () => reqEvent("join") (bot))
     bot.on("warn", reqEvent("warn"))
     bot.on("error", reqEvent("error"))
 }
