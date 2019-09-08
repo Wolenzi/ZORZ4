7a3bb1482c3b6c2a426f78571ecbf3b569714e9b
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-09-08 12:23:05 +0200

    Create invites.js

---
+++ commands/invites.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const colours = require("../colours.json");
+    arraySort = require('array-sort'),
+    table = require('table'),
+    send = require('quick.hook');
+
+module.exports.run = async (bot, message, args, tools) => {
+    let invites = await message.guid.fetchInvites().catch(error => {
+        return message.channel.send('Przepraszam, nie masz uprawnień do użycia tej komendy.');
+    })
+
+    invites = invites.array();
+
+    arraySort(invites, 'uses', { reverse: true });
+
+    let possibleInvites = [['User', 'Users']];
+    invites.forEach(function(invite) {
+        possibleInvites.push([invite.inviter.username, invite.uses]);
+    })
+
+    const embed = new Discord.MessageEmbed()
+        .setColor(colours.gold)
+        .setTitle('Server Invites')
+        .addField('TOP', `\`\`\`${table.table(possibleInvites)}\`\`\``);
+    message.channel.send(embed);    
+
+}
+
+module.exports.config = {
+    name: "invites",
+    aliases: ["inv", "zap"]
+}
