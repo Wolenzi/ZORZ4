210a08309969d15599c24d81d189e86cd7f2393c
Wolzi <49078678+Wolzi@users.noreply.github.com>
2019-09-08 21:11:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,8 +61,4 @@ bot.on("message", async message => {
 
 })
 
-bot.on("guildMemberAdd", (member) => {
-    member.guild.channels.find(c => c.name === "👋powitalnia").send(`"${member.user.username}" dołączył z zaproszenia "**${inviterName}**" (**${numInvites}** zaproszeń)`);
-});
-
 bot.login(process.env.token).catch(err => console.log(err));
