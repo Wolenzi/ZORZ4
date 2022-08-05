71838eeb5211d291a3288d7cf4c669ba9d8aa31b
Wolenzi <dawid.danelski@gmail.com>
2022-08-05 11:56:18 +0200

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -1,1 +1,36 @@
+<<<<<<< HEAD
 {}
+=======
+const Discord = require("discord.js");
+const colours = require("../colours.json");
+    arraySort = require('array-sort'),
+    table = require('table'),
+    send = require('quick.hook');
+
+module.exports.run = async (bot, message, args, tools) => {
+    let invites = await message.guild.fetchInvites().catch(error => {
+        return message.channel.send('Przepraszam, nie masz uprawnień do użycia tej komendy.');
+    })
+
+    invites = invites.array();
+
+    arraySort(invites, 'uses', { reverse: true });
+
+    let possibleInvites = [['Zaproszenie', 'Liczba użyć']];
+    invites.forEach(function(invite) {
+        possibleInvites.push([invite.inviter.username, invite.uses]);
+    })
+
+    let iembed = new Discord.RichEmbed()
+    .setColor(colours.gold)
+    .addField('Tabela Zaproszeń',`\`\`${table.table(possibleInvites)}\`\``);
+
+    message.channel.send(iembed);
+
+}
+
+module.exports.config = {
+    name: "invites",
+    aliases: ["in", "zap"]
+}
+>>>>>>> parent of d7387ac (Update invites.js)
